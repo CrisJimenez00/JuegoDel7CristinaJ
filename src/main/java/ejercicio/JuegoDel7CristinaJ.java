f2872201a40808e6e6987362eece443447c8edc8
CrisJimenez00 <cristina.del.carmen.jimenez.1@gmail.com>
2021-11-03 14:10:04 +0100

    Creación del proyecto y la clase

---
+++ src/main/java/ejercicio/JuegoDel7CristinaJ.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio;
+
+/**
+ *
+ * @author cristina
+ */
+public class JuegoDel7CristinaJ {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
